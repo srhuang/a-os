d6f2cf3565a62f61c879c140a1d5cbfd367234a1
srhuang <lukyandy3162@gmail.com>
2025-08-31 03:05:09 +0800

    ch03-7 load kernel.bin

--- boot/loader.s
+++ boot/loader.s
@@ -180,6 +180,14 @@ p_mode_start:
     call print_log
 
 ;-------------------------
+; load kernel
+;-------------------------
+    mov eax, KERNEL_START_SECTOR
+    mov ebx, KERNEL_BASE_ADDR
+    mov cl, KERNEL_SECTOR_COUNT
+    call read_hd_32
+
+;-------------------------
 ; pause the process
 ;-------------------------
     jmp $           ; stop here!!!
@@ -298,6 +306,72 @@ create_pte_loop:
     add eax, 0x1000
     inc esi
     loop create_pte_loop
+
+    ret
+
+;-------------------------
+; read hard disk
+;   eax : LBA
+;   ebx : RAM address
+;   ecx : sector count
+;-------------------------
+read_hd_32:
+
+; step 1 : set sector count
+    mov esi, eax    ; backup eax
+                    ; cause port operation only accept al/ax
+    mov dx, 0x1f2
+    mov al, cl
+    out dx, al
+    mov eax, esi    ; restore eax
+
+; step 2 : set LBA
+    ; LBA[7:0]
+    mov dx, 0x1f3
+    out dx, al
+
+    ; LBA[15:8]
+    shr eax, 8
+    mov dx, 0x1f4
+    out dx, al
+
+    ; LBA[23:16]
+    shr eax, 8
+    mov dx, 0x1f5
+    out dx, al
+
+    ; LBA[27:24] and setting
+    shr eax, 8
+    and al, 0x0f
+    or al, 0xe0     ; b'1110 for master drive / LBA addressing
+    mov dx, 0x1f6
+    out dx, al
+
+; step 3 : start read command
+    mov dx, 0x1f7
+    mov al,0x20
+    out dx, al
+
+; step 4 : polling status
+    polling:
+        nop             ; hardware delay
+        in al, dx
+        and al, 0x88    ; [4]:DRQ, [7]:BSY
+        cmp al, 0x08    ; Data is ready and not busy
+        jnz polling     ; keep polling
+
+; step 5 : read data from hard disk
+    mov al, cl
+    mov dx, 256     ; sector count * 512 / 2 
+                    ; (2 byte for each read)
+    mul dx          ; ax = ax * dx (only support ax)
+    mov cx, ax      ; cx = total loop time
+    mov dx, 0x1f0
+    read:
+        in ax, dx
+        mov [ebx], ax
+        add ebx, 2
+        loop read
 
     ret
 
