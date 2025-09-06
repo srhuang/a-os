e8b9c9e13367e306fd4a011d4416145d82cbb571
srhuang <lukyandy3162@gmail.com>
2025-09-07 02:52:27 +0800

    Task Gate

--- boot/loader.s
+++ boot/loader.s
@@ -38,7 +38,7 @@ a_stack_desc:DESCRIPTOR     0x0,        A_STACK_LEN-1,  GDT_G_1 \
                                                         + GDT_DPL_0 \
                                                         + GDT_S_SW \
                                                         + GDT_TYPE_DATA
-ldt_desc:DESCRIPTOR         0x0,        LDT_LEN-1,      GDT_G_1 \
+d_ldt_desc:DESCRIPTOR       0x0,        D_LDT_LEN-1,    GDT_G_1 \
                                                         + GDT_D_32 \
                                                         + GDT_L_32 \
                                                         + GDT_AVL_0 \
@@ -46,6 +46,34 @@ ldt_desc:DESCRIPTOR         0x0,        LDT_LEN-1,      GDT_G_1 \
                                                         + GDT_DPL_0 \
                                                         + GDT_S_HW \
                                                         + GDT_TYPE_LDT
+d_tss_desc:DESCRIPTOR       0x0,        D_TSS_LEN-1,    GDT_G_1 \
+                                                        + GDT_D_16 \
+                                                        + GDT_L_32 \
+                                                        + GDT_AVL_0 \
+                                                        + GDT_P_1 \
+                                                        + GDT_DPL_0 \
+                                                        + GDT_S_HW \
+                                                        + GDT_TYPE_TSS
+e_ldt_desc:DESCRIPTOR       0x0,        E_LDT_LEN-1,    GDT_G_1 \
+                                                        + GDT_D_32 \
+                                                        + GDT_L_32 \
+                                                        + GDT_AVL_0 \
+                                                        + GDT_P_1 \
+                                                        + GDT_DPL_0 \
+                                                        + GDT_S_HW \
+                                                        + GDT_TYPE_LDT
+e_tss_desc:DESCRIPTOR       0x0,        E_TSS_LEN-1,    GDT_G_1 \
+                                                        + GDT_D_16 \
+                                                        + GDT_L_32 \
+                                                        + GDT_AVL_0 \
+                                                        + GDT_P_1 \
+                                                        + GDT_DPL_0 \
+                                                        + GDT_S_HW \
+                                                        + GDT_TYPE_TSS
+; task gate             selector,attribute
+d_task_gate:TASK_GATE   SELECTOR_D_TSS, GDT_P_1 + GDT_DPL_0
+e_task_gate:TASK_GATE   SELECTOR_E_TSS, GDT_P_1 + GDT_DPL_0
+
 GDT_SIZE    equ $ - gdt_base
 GDT_LIMIT   equ GDT_SIZE - 1
 
@@ -53,10 +81,10 @@ gdtr    dw GDT_LIMIT    ; [15:0] gdt limit
         dd gdt_base     ; [47:16] gdt base
 
 ;-------------------------
-; LDT
+; D LDT
 ;-------------------------
 ;                       base,       limit,          attribute
-ldt_base:
+label_d_ldt_base:
 d_code_desc:DESCRIPTOR  0x0,        D_CODE_LEN-1,   GDT_G_1 \
                                                     + GDT_D_32 \
                                                     + GDT_L_32 \
@@ -81,7 +109,39 @@ d_stack_desc:DESCRIPTOR 0x0,        D_STACK_LEN-1,  GDT_G_1 \
                                                     + GDT_DPL_0 \
                                                     + GDT_S_SW \
                                                     + GDT_TYPE_DATA
-LDT_LEN  equ $ - ldt_base
+D_LDT_LEN  equ $ - label_d_ldt_base
+
+;-------------------------
+; E LDT
+;-------------------------
+;                       base,       limit,          attribute
+label_e_ldt_base:
+e_code_desc:DESCRIPTOR  0x0,        E_CODE_LEN-1,   GDT_G_1 \
+                                                    + GDT_D_32 \
+                                                    + GDT_L_32 \
+                                                    + GDT_AVL_0 \
+                                                    + GDT_P_1 \
+                                                    + GDT_DPL_0 \
+                                                    + GDT_S_SW \
+                                                    + GDT_TYPE_CODE
+e_data_desc:DESCRIPTOR  0x0,        E_DATA_LEN-1,   GDT_G_1 \
+                                                    + GDT_D_32 \
+                                                    + GDT_L_32 \
+                                                    + GDT_AVL_0 \
+                                                    + GDT_P_1 \
+                                                    + GDT_DPL_0 \
+                                                    + GDT_S_SW \
+                                                    + GDT_TYPE_DATA
+e_stack_desc:DESCRIPTOR 0x0,        E_STACK_LEN-1,  GDT_G_1 \
+                                                    + GDT_D_32 \
+                                                    + GDT_L_32 \
+                                                    + GDT_AVL_0 \
+                                                    + GDT_P_1 \
+                                                    + GDT_DPL_0 \
+                                                    + GDT_S_SW \
+                                                    + GDT_TYPE_DATA
+E_LDT_LEN  equ $ - label_e_ldt_base
+
 ;-------------------------
 ; GDT selector
 ;-------------------------
@@ -89,14 +149,88 @@ SELECTOR_VIDEO      equ (video_desc - gdt_base) + TI_GDT + RPL_0
 SELECTOR_A_CODE     equ (a_code_desc - gdt_base) + TI_GDT + RPL_0
 SELECTOR_A_DATA     equ (a_data_desc - gdt_base) + TI_GDT + RPL_0
 SELECTOR_A_STACK    equ (a_stack_desc - gdt_base) + TI_GDT + RPL_0
-SELECTOR_LDT        equ (ldt_desc - gdt_base) + TI_GDT + RPL_0
+SELECTOR_D_LDT      equ (d_ldt_desc - gdt_base) + TI_GDT + RPL_0
+SELECTOR_D_TSS      equ (d_tss_desc - gdt_base) + TI_GDT + RPL_0
+SELECTOR_D_TASK     equ (d_task_gate - gdt_base) + TI_GDT + RPL_0
+SELECTOR_E_LDT      equ (e_ldt_desc - gdt_base) + TI_GDT + RPL_0
+SELECTOR_E_TSS      equ (e_tss_desc - gdt_base) + TI_GDT + RPL_0
+SELECTOR_E_TASK     equ (e_task_gate - gdt_base) + TI_GDT + RPL_0
 
 ;-------------------------
 ; LDT selector
 ;-------------------------
-SELECTOR_D_CODE     equ (d_code_desc - ldt_base) + TI_LDT + RPL_0
-SELECTOR_D_DATA     equ (d_data_desc - ldt_base) + TI_LDT + RPL_0
-SELECTOR_D_STACK    equ (d_stack_desc - ldt_base) + TI_LDT + RPL_0
+SELECTOR_D_CODE     equ (d_code_desc - label_d_ldt_base) + TI_LDT + RPL_0
+SELECTOR_D_DATA     equ (d_data_desc - label_d_ldt_base) + TI_LDT + RPL_0
+SELECTOR_D_STACK    equ (d_stack_desc - label_d_ldt_base) + TI_LDT + RPL_0
+SELECTOR_E_CODE     equ (e_code_desc - label_e_ldt_base) + TI_LDT + RPL_0
+SELECTOR_E_DATA     equ (e_data_desc - label_e_ldt_base) + TI_LDT + RPL_0
+SELECTOR_E_STACK    equ (e_stack_desc - label_e_ldt_base) + TI_LDT + RPL_0
+
+;-------------------------
+; D TSS
+;-------------------------
+label_d_tss:
+    d_tss_backlink      dd 0x0
+    d_tss_esp0          dd A_STACK_TOP
+    d_tss_ss0           dd SELECTOR_A_STACK
+    d_tss_esp1          dd 0x0
+    d_tss_ss1           dd 0x0
+    d_tss_esp2          dd 0x0
+    d_tss_ss2           dd 0x0
+    d_tss_cr3           dd 0x0
+    d_tss_eip           dd 0x0
+    d_tss_eflags        dd 0x0
+    d_tss_eax           dd 0x0
+    d_tss_ecx           dd 0x0
+    d_tss_edx           dd 0x0
+    d_tss_ebx           dd 0x0
+    d_tss_esp           dd 0x0
+    d_tss_ebp           dd 0x0
+    d_tss_esi           dd 0x0
+    d_tss_edi           dd 0x0
+    d_tss_es            dd 0x0
+    d_tss_cs            dd SELECTOR_D_CODE
+    d_tss_ss            dd SELECTOR_D_STACK
+    d_tss_ds            dd SELECTOR_D_DATA
+    d_tss_fs            dd 0x0
+    d_tss_gs            dd 0x0
+    d_tss_ldt           dd SELECTOR_D_LDT
+    d_tss_trace         dw 0x0
+    d_tss_iobase        dw ($ - label_d_tss +2)
+D_TSS_LEN equ $ - label_d_tss
+
+;-------------------------
+; E TSS
+;-------------------------
+label_e_tss:
+    e_tss_backlink      dd 0x0
+    e_tss_esp0          dd A_STACK_TOP
+    e_tss_ss0           dd SELECTOR_A_STACK
+    e_tss_esp1          dd 0x0
+    e_tss_ss1           dd 0x0
+    e_tss_esp2          dd 0x0
+    e_tss_ss2           dd 0x0
+    e_tss_cr3           dd 0x0
+    e_tss_eip           dd 0x0
+    e_tss_eflags        dd 0x0
+    e_tss_eax           dd 0x0
+    e_tss_ecx           dd 0x0
+    e_tss_edx           dd 0x0
+    e_tss_ebx           dd 0x0
+    e_tss_esp           dd 0x0
+    e_tss_ebp           dd 0x0
+    e_tss_esi           dd 0x0
+    e_tss_edi           dd 0x0
+    e_tss_es            dd 0x0
+    e_tss_cs            dd SELECTOR_E_CODE
+    e_tss_ss            dd SELECTOR_E_STACK
+    e_tss_ds            dd SELECTOR_E_DATA
+    e_tss_fs            dd 0x0
+    e_tss_gs            dd 0x0
+    e_tss_ldt           dd SELECTOR_E_LDT
+    e_tss_trace         dw 0x0
+    e_tss_iobase        dw ($ - label_e_tss +2)
+E_TSS_LEN equ $ - label_e_tss
 
 ;-------------------------
 ; data
@@ -137,10 +271,16 @@ loader_start:
     DESCRIPTOR_SET_BASE a_code_desc,    cs,     label_a_code
     DESCRIPTOR_SET_BASE a_data_desc,    ds,     label_a_data
     DESCRIPTOR_SET_BASE a_stack_desc,   ss,     label_a_stack
-    DESCRIPTOR_SET_BASE ldt_desc,       cs,     ldt_base
     DESCRIPTOR_SET_BASE d_code_desc,    cs,     label_d_code
     DESCRIPTOR_SET_BASE d_data_desc,    ds,     label_d_data
     DESCRIPTOR_SET_BASE d_stack_desc,   ss,     label_d_stack
+    DESCRIPTOR_SET_BASE d_ldt_desc,     cs,     label_d_ldt_base
+    DESCRIPTOR_SET_BASE d_tss_desc,     cs,     label_d_tss
+    DESCRIPTOR_SET_BASE e_code_desc,    cs,     label_e_code
+    DESCRIPTOR_SET_BASE e_data_desc,    ds,     label_e_data
+    DESCRIPTOR_SET_BASE e_stack_desc,   ss,     label_e_stack
+    DESCRIPTOR_SET_BASE e_ldt_desc,     cs,     label_e_ldt_base
+    DESCRIPTOR_SET_BASE e_tss_desc,     cs,     label_e_tss
 
 ;-------------------------
 ; enable protected mode
@@ -174,7 +314,7 @@ A_DATA_LEN  equ $ - label_a_data
 label_a_stack:
     times 256 db 0
 A_STACK_LEN equ $ - label_a_stack
-A_STACK_TOP equ $ - label_a_stack - 1
+A_STACK_TOP equ A_STACK_LEN
 
 ;-------------------------
 ; A code
@@ -190,12 +330,8 @@ label_a_code:
     mov byte [gs:edi], bl
     mov byte [gs:edi+1], 0x4e
 
-    mov ax, SELECTOR_LDT
-    lldt ax
+    jmp SELECTOR_D_TASK:0
 
-    call SELECTOR_D_CODE:0
-
-    jmp $
 A_CODE_LEN  equ $ - label_a_code
 
 ;-------------------------
@@ -211,7 +347,7 @@ D_DATA_LEN  equ $ - label_d_data
 label_d_stack:
     times 256 db 0
 D_STACK_LEN equ $ - label_d_stack
-D_STACK_TOP equ $ - label_d_stack - 1
+D_STACK_TOP equ D_STACK_LEN
 
 ;-------------------------
 ; D code
@@ -227,6 +363,50 @@ label_d_code:
     mov byte [gs:edi], bl
     mov byte [gs:edi+1], 0x4e
 
-    retf
+    call SELECTOR_E_TASK:0
+
+; print log
+    mov ax, SELECTOR_VIDEO
+    mov gs, ax
+    mov edi, (80*20)*2
+    mov ax, SELECTOR_D_DATA
+    mov ds, ax
+    mov bl, [ds:0]
+    mov byte [gs:edi], bl
+    mov byte [gs:edi+1], 0x4e
+
+    jmp $
+
 D_CODE_LEN  equ $ - label_d_code
 
+;-------------------------
+; E data
+;-------------------------
+label_e_data:
+    db "Egg"
+E_DATA_LEN  equ $ - label_e_data
+
+;-------------------------
+; E stack
+;-------------------------
+label_e_stack:
+    times 256 db 0
+E_STACK_LEN equ $ - label_e_stack
+E_STACK_TOP equ E_STACK_LEN
+
+;-------------------------
+; E code
+;-------------------------
+label_e_code:
+; print log
+    mov ax, SELECTOR_VIDEO
+    mov gs, ax
+    mov edi, (80*19)*2
+    mov ax, SELECTOR_E_DATA
+    mov ds, ax
+    mov bl, [ds:0]
+    mov byte [gs:edi], bl
+    mov byte [gs:edi+1], 0x4e
+    
+    iretd
+E_CODE_LEN  equ $ - label_e_code
